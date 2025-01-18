e090edc76c514d5282a6a35afa1caba477c7c6d4
Mikael Andersson <mikael.l.andersson91@gmail.com>
2025-01-18 11:39:02 +0100

    feat: write failing commits to step summary

--- pkg/text/format_failing_commits.go
+++ pkg/text/format_failing_commits.go
@@ -2,6 +2,8 @@ package text
 
 import (
 	"strings"
+	"fmt"
+	"os"
 
 	"github.com/jedib0t/go-pretty/table"
 )
@@ -26,6 +28,38 @@ func FormatFailingCommits(commits []FailingCommit) table.Writer {
 		t.AppendRow(table.Row{commit.Hash, commit.Error.Error(), commit.Message})
 	}
 
+	// Generate markdown table
+	markdownTable := generateMarkdownTable(commits)
+
+	// Write markdown table to GITHUB_STEP_SUMMARY
+	writeToGitHubStepSummary(markdownTable)
+
 	return t
+}
+
+func generateMarkdownTable(commits []FailingCommit) string {
+    var sb strings.Builder
+    sb.WriteString("| hash | failure | text |\n")
+    sb.WriteString("|------|---------|------|\n")
+    for _, commit := range commits {
+        sb.WriteString(fmt.Sprintf("| %s | %s | %s |\n", commit.Error.Error(), strings.TrimSpace(commit.Message)))
+    }
+    return sb.String()
+}
+
+func writeToGitHubStepSummary(content string) {
+    summaryFile := os.Getenv("GITHUB_STEP_SUMMARY")
+    if summaryFile != "" {
+        f, err := os.OpenFile(summaryFile, os.O_APPEND|os.O_WRONLY, 0600)
+        if err != nil {
+            fmt.Printf("Error opening GITHUB_STEP_SUMMARY file: %v\n", err)
+            return
+        }
+        defer f.Close()
+
+        if _, err = f.WriteString(content); err != nil {
+            fmt.Printf("Error writing to GITHUB_STEP_SUMMARY file: %v\n", err)
+        }
+    }
 }
 
